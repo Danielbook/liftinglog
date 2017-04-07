b9502edf168da64e4cdf1266d6664b6da9dd5d9f
Daniel Böök <danbo324@student.liu.se>
2017-04-07 10:11:25 +0200

    Moved on to material ui instead of semantic

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -1,22 +1,37 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-
+import React, {Component, PropTypes} from "react";
+import {connect} from "react-redux";
 // Import Components
-import Helmet from 'react-helmet';
-import DevTools from './components/DevTools';
+import Helmet from "react-helmet";
+import DevTools from "./components/DevTools";
 // import Header from './components/Header/Header';
 // import Footer from './components/Footer/Footer';
 
-// Import Actions
-import { toggleAddPost } from './AppActions';
-// import { switchLanguage } from '../../modules/Intl/IntlActions';
+import Navbar from './Navbar';
+import Sidebar from "./Sidebar";
 
-import { Button, Container, Header } from 'semantic-ui-react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {toggleAddPost} from "./AppActions";
+import RaisedButton from "material-ui/RaisedButton";
+
+import injectTapEventPlugin from "react-tap-event-plugin";
+// import {Button, Header, Icon, Menu, Segment, Sidebar} from "semantic-ui-react";
+// import { switchLanguage } from '../../modules/Intl/IntlActions';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 export class App extends Component {
+  toggleVisibility = () => this.setState({visible: !this.state.visible});
+
   constructor(props) {
     super(props);
-    this.state = { isMounted: false };
+    //this.handleTouchTap = this.handleTouchTap.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.state = {
+      isMounted:  false,
+      visible:    true,
+      sideBarOpen: false
+    };
   }
 
   componentDidMount() {
@@ -27,46 +42,64 @@ export class App extends Component {
     this.props.dispatch(toggleAddPost());
   };
 
-  render() {
-    return (
-      <div>
-        {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
-        <div>
-          <Helmet
-            title="Lifting Log"
-            meta={[
-              { charset: 'utf-8' },
-              {
-                'http-equiv': 'X-UA-Compatible',
-                content: 'IE=edge',
-              },
-              {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1',
-              },
-            ]}
-          />
-          <Container>
-            <Header as='h1'>Hello world!</Header>
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  }
 
-            <Button
-              content='Discover docs'
-              href='http://react.semantic-ui.com'
-              icon='github'
-              labelPosition='left'
+
+  handleToggle = () => this.setState({sideBarOpen: !this.state.sideBarOpen});
+
+  logging = () => console.log(this.state);
+
+  render() {
+    const {visible} = this.state;
+    return (
+      <MuiThemeProvider>
+        <div>
+          {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
+          <div>
+            <Helmet
+              title="Lifting Log"
+              meta={[
+                {charset: 'utf-8'},
+                {
+                  'http-equiv': 'X-UA-Compatible',
+                  content:      'IE=edge',
+                },
+                {
+                  name:    'viewport',
+                  content: 'width=device-width, initial-scale=1',
+                },
+              ]}
             />
-          </Container>
-          {/*<Header*/}
+            <Navbar />
+
+            <Sidebar open={this.state.sideBarOpen}/>
+
+            <RaisedButton
+              label="Toggle Drawer"
+              onTouchTap={this.handleToggle}
+            />
+
+            <RaisedButton
+              label="Log state"
+              onTouchTap={this.logging}
+            />
+
+            {/*<Header*/}
             {/*switchLanguage={lang => this.props.dispatch(switchLanguage(lang))}*/}
             {/*intl={this.props.intl}*/}
             {/*toggleAddPost={this.toggleAddPostSection}*/}
-          {/*/>*/}
-          {/*<div className={styles.container}>*/}
+            {/*/>*/}
+            {/*<div className={styles.container}>*/}
             {/*{this.props.children}*/}
-          {/*</div>*/}
-          {/*<Footer />*/}
+            {/*</div>*/}
+            {/*<Footer />*/}
+          </div>
         </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
@@ -74,7 +107,7 @@ export class App extends Component {
 App.propTypes = {
   children: PropTypes.object.isRequired,
   dispatch: PropTypes.func.isRequired,
-  intl: PropTypes.object.isRequired,
+  intl:     PropTypes.object.isRequired,
 };
 
 // Retrieve data from store as props
