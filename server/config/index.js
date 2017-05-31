58363b2876a2330fb8fd14a305102d95b60cc2d3
Daniel Böök <Danbo324@student.liu.se>
2017-05-31 11:43:45 +0200

    Changed to remote DB

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: 'mongodb://localhost/lifting_log' || process.env.ATLAS_URL,
+  mongoURL: process.env.ATLAS_URL || 'mongodb://localhost/lifting_log',
   PORT: process.env.PORT || 8000,
   jwtSecret: "a secret phrase!!",
 };
